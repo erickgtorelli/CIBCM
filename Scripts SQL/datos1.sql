48037e2994c4fb3e942367b8475a4183f83bd0e4
unknown <b32831@ECCI-LBW-106-15.ecci.ucr.ac.cr>
2015-06-29 11:01:44 -0600

    agregar scripts

--- Scripts SQL/datos1.sql
+++ Scripts SQL/datos1.sql
@@ -1,4 +1,4 @@
-Insert into Persona values('115825968','Paco','Mora','Torres','19980901','M');
+Insert into Persona values('115825968','Francisco','Mora','Torres','19980901','M');
 Insert into Persona values('118258695','Juana','Arce','Ballester','19900901','F');
 Insert into Persona values('116485796','Gloriana','Porras','Bustos','19980703','F');
 Insert into Persona values('113258695','Carolina','Arroyo','Tristan','19950901','F');
@@ -28,6 +28,14 @@ Insert into Investigador values('114458968');
 Insert into Investigador values('118568695');
 
 Insert into Diagnostico values('115525968','1','/CIBCM/diagnosticoAlejandro.doc','20000815','Esquizofrenia');
-Insert into Parcial values('115525968','1','110000095');
-Insert into Diagnostico values('115525968','1','/CIBCM/diagnosticoAlejandro.doc','20000815','Esquizofrenia');
+Insert into Parcial values('115525968','1','110000095',null,null);
+Insert into Diagnostico values('113258695','2','/CIBCM/diagnosticoCarolina.doc','20000817','Bipolaridad');
+Insert into Parcial values('113258695','2','118568695',null,null);
+
+Insert into InstrumentosClinicos VALUES ('DIGS'), ('FIGS'), ('AIMS'), ('Expediente médico'), ('STAN');
+
+INSERT INTO Genotipeo VALUES ('115525968', 'Secuenciación', '/CIBCM/115525968/Genotipeo'), ('118268695', 'SNP', '/CIBCM/118268695/Genotipeo');
+
+Insert Into Estudio VALUES ('123987', 'SIBS bipolar', '20090407');
+Insert Into Realiza   ('118568695','123987');
 
